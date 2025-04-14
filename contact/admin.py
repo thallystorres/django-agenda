da4a981493c78812eb0fb9bf261982a53ab2f514
thallys <thallys.auugusto.torres@gmail.com>
2025-04-14 01:28:31 -0300

    Criando a zona de tabela

--- contact/admin.py
+++ contact/admin.py
@@ -5,11 +5,12 @@ from contact import models
 
 @admin.register(models.Contact)
 class Contact(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name',
+    list_display = 'id', 'first_name', 'last_name', 'show'
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name'
     list_per_page = 20
     list_max_show_all = 100
+    list_editable = 'show',
     list_display_links = 'first_name', 'last_name',
 
 
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from contact import models
 
 
 def index(request):
+    contacts = models.Contact.objects \
+    .filter(show=True) \
+    .order_by('-id')
+    context = {
+        'contacts': contacts,
+    }
     return render(
         request,
         'contact/index.html',
+        context=context
         )
