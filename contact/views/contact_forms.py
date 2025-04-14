f768366d6f69bd36be646b54cf2ea62118e4c60e
thallys <thallys.auugusto.torres@gmail.com>
2025-04-14 15:03:40 -0300

    Diferenciação de metodos POST e GET

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 from django.shortcuts import render, get_object_or_404, redirect
 from contact import models
 from django.db.models import Q
@@ -5,6 +6,8 @@ from django.core.paginator import Paginator
 
 
 def create(request):
+    if request.method == 'POST':
+        print('FOI POST RAPA')
     context = {}
     return render(
         request,
