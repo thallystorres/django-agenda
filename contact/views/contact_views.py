7899631baef71a6958f6420e9729773da33c1783
thallys <thallys.auugusto.torres@gmail.com>
2025-04-14 02:48:51 -0300

    Criando funcionalidade de busca simples por query

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact import models
+from django.db.models import Q
 
 
 def index(request):
@@ -9,6 +10,32 @@ def index(request):
     context = {
         'contacts': contacts,
         'site_title': 'Contatos -',
+    }
+    return render(
+        request,
+        'contact/index.html',
+        context=context
+        )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = models.Contact.objects \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+            ) \
+        .order_by('-id')
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search -',
     }
     return render(
         request,
