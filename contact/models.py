a41de79206c19a1da59f32bb944e2b26182aa78e
thallys <thallys.auugusto.torres@gmail.com>
2025-04-13 23:03:24 -0300

    Criando ImageField e configurando MEDIA_URL, MEDIA_ROOT, STATIC_URL e STATIC_ROOT

--- contact/models.py
+++ contact/models.py
@@ -6,9 +6,10 @@ from django.utils import timezone
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
 
+# category (foreign key), show (boolean), picture (imagem)
+
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# owner (foreign key)
 
 
 class Contact(models.Model):
@@ -18,6 +19,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)  # type: ignore
     created_date = models.DateTimeField(default=timezone.now)  # type: ignore
     description = models.TextField(blank=True)  # type: ignore
+    show = models.BooleanField(default=True)  # type: ignore
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
--- project/settings.py
+++ project/settings.py
@@ -117,12 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'  # collectstatic
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # collectmedia
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
