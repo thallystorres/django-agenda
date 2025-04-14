d5ed3a0becf9472bc0ce8d343f3d707c5313624a
thallys <thallys.auugusto.torres@gmail.com>
2025-04-13 22:25:11 -0300

    Manipulando e atualizando a admin area do site

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from contact import models
 # Register your models here.
+
+
+@admin.register(models.Contact)
+class Contact(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name',
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name'
+    list_per_page = 20
+    list_max_show_all = 100
+    list_display_links = 'first_name', 'last_name',
--- contact/models.py
+++ contact/models.py
@@ -12,9 +12,12 @@ from django.utils import timezone
 
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50, blank=True)
-    phone = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254, blank=True)
-    created_date = models.DateTimeField(default=timezone.now)
-    description = models.TextField(blank=True)
+    first_name = models.CharField(max_length=50)  # type: ignore
+    last_name = models.CharField(max_length=50, blank=True)  # type: ignore
+    phone = models.CharField(max_length=50)  # type: ignore
+    email = models.EmailField(max_length=254, blank=True)  # type: ignore
+    created_date = models.DateTimeField(default=timezone.now)  # type: ignore
+    description = models.TextField(blank=True)  # type: ignore
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
