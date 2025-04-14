9a0e1b2ab35a790713ab92e591a9debe6fa3435f
thallys <thallys.auugusto.torres@gmail.com>
2025-04-14 18:24:08 -0300

    Criando arquivo forms.py

---
+++ contact/forms.py
@@ -0,0 +1,20 @@
+from django.core.exceptions import ValidationError
+from django import forms
+from contact.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = 'first_name', 'last_name', 'phone', 'email',
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        return super().clean()
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,28 +1,6 @@
-# flake8: noqa
-from typing import Any
-from django.shortcuts import render, get_object_or_404, redirect
-from contact import models
-from django.db.models import Q
-from django.core.paginator import Paginator
-from django.core.exceptions import ValidationError
-from django import forms
-from contact.models import Contact
+from django.shortcuts import render
+from contact.forms import ContactForm
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = 'first_name', 'last_name', 'phone', 'email',
-        
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        return super().clean()
 
 def create(request):
     if request.method == 'POST':
