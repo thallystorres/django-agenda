0d93aa06ec65ed7c565906e517508802ab02c121
thallys <thallys.auugusto.torres@gmail.com>
2025-04-14 18:20:00 -0300

    Tratando erros do formulário

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,8 +1,10 @@
 # flake8: noqa
+from typing import Any
 from django.shortcuts import render, get_object_or_404, redirect
 from contact import models
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django.core.exceptions import ValidationError
 from django import forms
 from contact.models import Contact
 
@@ -10,6 +12,17 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = 'first_name', 'last_name', 'phone', 'email',
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        return super().clean()
 
 def create(request):
     if request.method == 'POST':
