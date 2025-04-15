658c0dc3660aba4e012129d4c49e0c2436d3f3de
thallys <thallys.auugusto.torres@gmail.com>
2025-04-15 13:06:19 -0300

    Usando decorator login_required para barrar usuários não logados

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
 from django.urls import reverse
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
     if request.method == 'POST':
@@ -37,6 +39,7 @@ def create(request):
         )
 
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
@@ -73,6 +76,7 @@ def update(request, contact_id):
         )
 
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -3,6 +3,7 @@ from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -49,12 +50,14 @@ def login_view(request):
     )
 
 
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     messages.info(request, 'Logout successful.')
     return redirect('contact:login')
 
 
+@login_required(login_url='contact:login')
 def user_update(request):
     form = RegisterUpdateForm(instance=request.user)
 
