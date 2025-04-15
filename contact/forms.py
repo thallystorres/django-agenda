5f4aad8ecf350f87e114edb8ef67cd7d866cf367
thallys <thallys.auugusto.torres@gmail.com>
2025-04-14 23:44:09 -0300

    Validação de campos de nome e email

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 from django import forms
 from contact.models import Contact
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ContactForm(forms.ModelForm):
@@ -33,4 +34,29 @@ class ContactForm(forms.ModelForm):
 
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username',
+                  'password1', 'password2',
+                  )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Esse email já existe', code='invalid')
+            )
+
+        return email
