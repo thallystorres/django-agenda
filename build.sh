f82d4daeadb78e6afd8543232d2fd1d0cc97a84e
thallys <thallys.auugusto.torres@gmail.com>
2025-04-15 18:50:36 -0300

    removendo arquivos desnecessários

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Instalar dependências
-pip install -r requirements.txt
-
-# Coletar arquivos estáticos
-python manage.py collectstatic --noinput
-
-# Aplicar migrações
-python manage.py migrate
