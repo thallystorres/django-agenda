420b4a958a912363e8927f0d2950bc70164f73a0
thallys <thallys.auugusto.torres@gmail.com>
2025-04-14 21:57:54 -0300

    Criando lógica da página de update de contatos

--- contact/urls.py
+++ contact/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # CRUD de contatos
     path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update', views.update, name='update'),
     path('contact/create/', views.create, name='create'),
 ]
