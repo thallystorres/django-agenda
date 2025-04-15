0e98a49c17c3941593479e26d39af4b7734e8030
thallys <thallys.auugusto.torres@gmail.com>
2025-04-15 11:09:43 -0300

    Criando lógica de login e logout

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def register(request):
@@ -9,9 +11,9 @@ def register(request):
         form = RegisterForm(request.POST)
 
         if form.is_valid():
-            messages.success(request, 'Registered user')
             form.save()
-            redirect('contact:index')
+            messages.success(request, 'Registered user')
+            return redirect('contact:login')
 
     context = {
         'form': form
@@ -22,3 +24,31 @@ def register(request):
         'contact/register.html',
         context=context,
     )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            messages.success(request, 'Successfully logged in')
+            auth.login(request, user)
+            return redirect('contact:index')
+        messages.error(request, 'Invalid login')
+    context = {
+        'form': form,
+    }
+
+    return render(
+        request,
+        'contact/login.html',
+        context=context,
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
